040bd6552023fe2a3827878047dfa40529769218
Mildred <99267408+mildrette@users.noreply.github.com>
2024-04-05 11:03:26 +0100

    added correct links to the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,18 +4,23 @@ import { FaFacebook, FaGithub, FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
 	return (
-		<Box component="footer" sx={{ bgcolor: "dark" }}>
-			<Grid container sx={{ p: 2, position: "sticky", bottom: 0 }}>
+		<Box component="footer" sx={{ position: "relative", bottom: 0 }}>
+			<Grid container sx={{ p: 2 }}>
 				<Grid item md={2}></Grid>
-
-				<Grid item md={9} sm={12} xs={12}>
+				<Grid item md={9}>
 					<Divider sx={{ width: "100%" }} />
-					<Box display="flex" justifyContent="space-between" sx={{ p: 3 }}>
+					<Box display="flex" justifyContent="space-between" sx={{ p: 2 }}>
 						<Typography variant="body2">© {new Date().getFullYear()} SMSWithoutBorders</Typography>
 						<Box justifyContent="end">
-							<FaGithub size="20px" style={{ margin: 2 }} />{" "}
-							<FaFacebook size="20px" style={{ margin: 2 }} />{" "}
-							<FaXTwitter size="20px" style={{ margin: 2 }} />
+							<a href="https://github.com/smswithoutborders">
+								<FaGithub size="20px" style={{ margin: 2 }} />
+							</a>{" "}
+							<a href="https://web.facebook.com/SMSWithoutBorders?_rdc=1&_rdr">
+								<FaFacebook size="20px" style={{ margin: 2 }} />
+							</a>{" "}
+							<a href="https://twitter.com/SwobOutreach">
+								<FaXTwitter size="20px" style={{ margin: 2 }} />
+							</a>
 						</Box>
 					</Box>
 				</Grid>
