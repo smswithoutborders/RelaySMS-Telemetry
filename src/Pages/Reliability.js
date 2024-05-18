8e735f737cdddd66eb9be90acfc686aa16d0c4ec
mildrette <mildredfonka1@gmail.com>
2024-05-18 16:58:46 +0100

    changed resilience to reliability on the Reliability table

--- src/Pages/Reliability.js
+++ src/Pages/Reliability.js
@@ -68,7 +68,7 @@ export default function Reliability() {
 		{ field: "msisdn", headerName: "MSISDN", width: 200 },
 		{ field: "country", headerName: "Country", width: 200 },
 		{ field: "operator", headerName: "Operator", width: 200 },
-		{ field: "resiliance", headerName: "Reliability", width: 200 },
+		{ field: "reliability", headerName: "Reliability", width: 200 },
 		{ field: "date", headerName: "Date/Time", width: 200 }
 	];
 
