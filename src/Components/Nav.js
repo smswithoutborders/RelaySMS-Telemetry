013503c58b585a50a267481f69e4c7b278b4b908
mildrette <mildredfonka1@gmail.com>
2025-01-24 02:36:01 +0100

    update to navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,148 +1,212 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import {
 	Box,
-	CssBaseline,
 	Drawer,
 	List,
 	ListItem,
-	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	IconButton,
 	Typography,
-	Divider,
-	Paper,
-	useTheme,
-	Button
+	Tooltip,
+	Button,
+	useMediaQuery
 } from "@mui/material";
-import { FaCircleQuestion, FaHeadphones, FaTable, FaTableCells } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import {
+	FaArrowLeft,
+	FaArrowRight,
+	FaEnvelope,
+	FaRegHandshake,
+	FaShieldAlt,
+	FaBroadcastTower,
+	FaPhoneAlt
+} from "react-icons/fa";
+import { ChevronRight } from "@mui/icons-material";
 
-const drawerWidth = 280;
+const drawerWidth = 200;
 
-function ResponsiveDrawer() {
-	const theme = useTheme();
+function Sidebar() {
+	const [isCollapsed, setIsCollapsed] = useState(false);
 
-	const drawer = (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				height: "100%",
-				bgcolor: theme.palette.background.default,
-				color: theme.palette.text.primary,
-				boxShadow: theme.shadows[1],
-				borderRadius: "0 16px 16px 0",
-				overflow: "hidden"
-			}}
-		>
-			{/* Header */}
-			<Box
-				display="flex"
-				alignItems="center"
-				sx={{
-					p: 3,
-					bgcolor: theme.palette.primary.main,
-					color: theme.palette.primary.contrastText,
-					borderRadius: "0 16px 0 0"
-				}}
-			>
-				<Box component="img" src="/logo.png" sx={{ width: "40px", borderRadius: "50%" }} />
-				<Typography variant="h6" sx={{ px: 2, fontWeight: "bold" }}>
-					SWOB Dashboard
-				</Typography>
-			</Box>
+	const handleToggle = () => {
+		setIsCollapsed(!isCollapsed);
+	};
 
-			<Divider sx={{ my: 2 }} />
+	// Media query to detect mobile and tablet screens
+	const isMobileOrTablet = useMediaQuery((theme) => theme.breakpoints.down("md")); // For screens smaller than or equal to 'md' (900px)
 
-			{/* Navigation Links */}
-			<List sx={{ flexGrow: 1, px: 1 }}>
-				{[
-					{ text: "Reliability", icon: <FaTableCells />, to: "/" },
-					{ text: "Resilience", icon: <FaTable />, to: "/resilience" },
-					{ text: "OpenTelemetry", icon: <FaTable />, to: "/OpenTelemetry" },
-					{ text: "Help", icon: <FaCircleQuestion />, to: "/help" },
-					{ text: "Contact", icon: <FaHeadphones />, to: "/contact" }
-				].map((item, index) => (
-					<ListItem key={index} disablePadding>
-						<ListItemButton
-							component={Link}
-							to={item.to}
-							sx={{
-								borderRadius: "8px",
-								mx: 2,
-								my: 1,
-								"&:hover": {
-									bgcolor: theme.palette.action.hover
-								}
-							}}
-						>
-							<ListItemIcon sx={{ color: theme.palette.text.secondary }}>{item.icon}</ListItemIcon>
-							<ListItemText primary={item.text} />
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
+	const menuItems = [
+		{ text: "Reliability", icon: <FaRegHandshake />, link: "/" },
+		{ text: "Resilience", icon: <FaShieldAlt />, link: "/resilience" },
+		{ text: "Open Telemetry", icon: <FaBroadcastTower />, link: "/OpenTelemetry" }
+	];
 
-			<Divider sx={{ my: 2 }} />
-
-			{/* Footer */}
-			<Box sx={{ p: 3 }}>
-				<Paper
-					elevation={2}
-					sx={{
-						p: 2,
-						textAlign: "center",
-						borderRadius: "12px",
-						bgcolor: theme.palette.background.paper,
-						color: theme.palette.text.primary
-					}}
-				>
-					<Typography variant="body1" sx={{ fontWeight: "bold", mb: 1 }}>
-						Check out SMSWithoutBorders blog posts
-					</Typography>
-					<Button
-						component="a"
-						href="https://blog.smswithoutborders.com/"
-						target="_blank"
-						rel="noopener noreferrer"
-						variant="contained"
-						color="primary"
-						sx={{ borderRadius: "50px", px: 3 }}
-					>
-						Read More
-					</Button>
-				</Paper>
-			</Box>
-		</Box>
-	);
+	const serviceItems = [
+		{ text: "Help", icon: <FaEnvelope />, link: "/help" },
+		{ text: "Contact Us", icon: <FaPhoneAlt />, link: "/contact" }
+	];
 
 	return (
-		<Box sx={{ display: { xs: "none", md: "flex" } }}>
-			{" "}
-			{/* Hide on mobile */}
-			<CssBaseline />
+		<Box sx={{ display: "flex" }}>
+			{/* Hide the Drawer on mobile and tablet */}
 			<Drawer
 				variant="permanent"
 				sx={{
-					width: drawerWidth,
+					width: isMobileOrTablet ? 0 : isCollapsed ? 40 : drawerWidth, // Hide on mobile and tablet
 					flexShrink: 0,
 					"& .MuiDrawer-paper": {
-						width: drawerWidth,
+						width: isMobileOrTablet ? 0 : isCollapsed ? 60 : drawerWidth, // Hide on mobile and tablet
 						boxSizing: "border-box",
-						bgcolor: theme.palette.background.default,
-						borderRight: `1px solid ${theme.palette.divider}`
+						transition: "width 0.3s",
+						backdropFilter: "blur(10px)",
+						borderRight: "1px solid rgba(255, 255, 255, 0.5)"
 					}
 				}}
 			>
-				{drawer}
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						height: "100%",
+						justifyContent: "space-between"
+					}}
+				>
+					{/* Header */}
+					<Box sx={{ padding: 2, display: "flex", alignItems: "center" }}>
+						{!isCollapsed && !isMobileOrTablet && (
+							<Box
+								sx={{
+									display: "flex",
+									alignItems: "center",
+									flexGrow: 1
+								}}
+							>
+								<Box
+									component="img"
+									src="/logo.png"
+									alt="Profile"
+									sx={{
+										width: 30,
+										height: 30,
+										borderRadius: "50%",
+										marginRight: 1
+									}}
+								/>
+								<Box>
+									<Typography variant="body2" sx={{ fontWeight: 600 }}>
+										RelaySMS
+									</Typography>
+								</Box>
+							</Box>
+						)}
+						<IconButton onClick={handleToggle}>
+							{isCollapsed ? <FaArrowRight /> : <FaArrowLeft />}
+						</IconButton>
+					</Box>
+
+					{/* Menu Section */}
+					<Box>
+						<Typography variant="caption" sx={{ paddingLeft: 2, color: "gray" }}>
+							Menus
+						</Typography>
+						<List>
+							{menuItems.map((item, index) => (
+								<Tooltip title={isCollapsed ? item.text : ""} placement="right" key={index}>
+									<ListItem
+										button
+										component={NavLink}
+										to={item.link}
+										sx={{
+											padding: "10px",
+											justifyContent: isCollapsed ? "center" : "flex-start"
+										}}
+									>
+										<ListItemIcon
+											sx={{
+												minWidth: 0,
+												marginRight: isCollapsed ? 0 : 2,
+												justifyContent: "center"
+											}}
+										>
+											{item.icon}
+										</ListItemIcon>
+										{!isCollapsed && <ListItemText primary={item.text} />}
+									</ListItem>
+								</Tooltip>
+							))}
+						</List>
+
+						<Typography variant="caption" sx={{ paddingLeft: 2, color: "gray" }}>
+							Contact
+						</Typography>
+						<List>
+							{serviceItems.map((item, index) => (
+								<Tooltip title={isCollapsed ? item.text : ""} placement="right" key={index}>
+									<ListItem
+										button
+										component={NavLink}
+										to={item.link}
+										style={({ isActive }) => ({
+											padding: "10px",
+											justifyContent: isCollapsed ? "center" : "flex-start",
+											backgroundColor: isActive ? "rgba(0, 123, 255, 0.2)" : "transparent"
+										})}
+									>
+										<ListItemIcon
+											sx={{
+												minWidth: 0,
+												marginRight: isCollapsed ? 0 : 2,
+												justifyContent: "center"
+											}}
+										>
+											{item.icon}
+										</ListItemIcon>
+										{!isCollapsed && <ListItemText primary={item.text} />}
+									</ListItem>
+								</Tooltip>
+							))}
+						</List>
+					</Box>
+
+					{/* Footer */}
+					<Box sx={{ padding: 2 }}>
+						<ListItem
+							button
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+								backgroundColor: "rgba(97, 112, 196, 0.1)",
+								borderRadius: 2,
+								boxShadow: "1px 1px 10px rgba(102, 99, 99, 0.1)"
+							}}
+						>
+							{!isCollapsed && !isMobileOrTablet && (
+								<>
+									<Typography variant="body2" sx={{ marginTop: 1 }}>
+										Check out RelaySMS blog posts
+									</Typography>
+									<Typography variant="caption" color="textSecondary">
+										<Button
+											component="a"
+											href="https://blog.smswithoutborders.com/"
+											rel="noreferrer"
+											target="_blank"
+											variant="contained"
+											sx={{ mt: 2, borderRadius: "50px", textTransform: "none" }}
+										>
+											Read more <ChevronRight />
+										</Button>
+									</Typography>
+								</>
+							)}
+						</ListItem>
+					</Box>
+				</Box>
 			</Drawer>
 		</Box>
 	);
 }
 
-ResponsiveDrawer.propTypes = {
-	window: PropTypes.func
-};
-
-export default ResponsiveDrawer;
+export default Sidebar;
