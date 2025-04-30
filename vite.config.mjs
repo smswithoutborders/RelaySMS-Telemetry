23310a4cf56920a6b8e55ddd31d54b64df476cbc
Promise Fru <info@promisefru.com>
2025-04-30 21:55:04 +0100

    fix: update deployment pipeline and nginx configuration for improved environment handling and routing

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = 3000;
 
   return {
@@ -39,7 +37,6 @@ export default defineConfig(({ mode }) => {
         // },
       ]
     },
-    base: API_URL,
     plugins: [react(), jsconfigPaths()]
   };
 });
